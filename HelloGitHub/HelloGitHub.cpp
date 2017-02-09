3b654e7bec8d8f4d3b2101852e758747fab5be62
polmolesrivera <polmolesrivera@S3-27>
2017-02-09 13:54:08 +0100

    Print hello world

--- HelloGitHub/HelloGitHub.cpp
+++ HelloGitHub/HelloGitHub.cpp
@@ -1,11 +1,14 @@
 // HelloGitHub.cpp : Defines the entry point for the console application.
 //
+#include<iostream>
 
 #include "stdafx.h"
 
+using namespace std;
 
 int main()
 {
+	cout << "Hello World" << endl;
     return 0;
 }
 
